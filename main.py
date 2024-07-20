58820925e4a51d84abfb98728be3933ff6f4e4a1
skbootdev <skbootdev@fastmail.net>
2024-07-20 15:26:21 -0500

    add modified main.py

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-    print(file_contents)
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    print(text)
 
-if __name__ == "__main__":
-    main()
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+main()
 
